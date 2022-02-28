3dfc45d1c38264f0f2cc0940614d0c30812e9be3
shuhongfan <shuhongfan@live.com>
2022-02-28 10:51:00 +0800

    2022.2.28 TextView

---
+++ MyApplication/app/src/main/java/com/shf/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.shf.myapplication;
+
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View tv_one = findViewById(R.id.tv_one);
+
+    }
+}
